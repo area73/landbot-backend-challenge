4ab7016a0865546c23011a68a0cbb0aad52e8858
rerades <rerades@siete3.com>
2024-11-17 12:58:57 +0100

    adding coverage and missing test

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -4,7 +4,6 @@ const notificationService = new NotificationService();
 
 export const notify = async (req: Request, res: Response) => {
   const { topic, message } = req.body;
-
   try {
     await notificationService.notify(topic, message);
     res.status(200).json({ message: `Notification sent to ${topic} channel` });
